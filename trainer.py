de1c955538e2e576960c3f46e4f04326b906f858
euclaise <jpritsk@purdue.edu>
2023-10-03 07:55:31 -0400

    eh

--- trainer.py
+++ trainer.py
@@ -54,7 +54,7 @@ class SlimTrainer():
                     if self.wandb_entity is not None:
                         wandb.log({'loss': loss.item()})
                         wandb.log({'epoch': epoch + batch_idx / total_batches})
-                        wandb.log({'step': epoch*total_batches*self.batch_size + batch_idx*self.batch_size})
+                        wandb.log({'step': epoch*self.batch_size + batch_idx})
                         wandb.log({'learning_rate': self.scheduler.get_lr()})
 
             if hasattr(self.scheduler, "epoch_end"):
