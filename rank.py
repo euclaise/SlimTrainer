0c93b05c667c67d6004a5d7b719c30403fed79fd
euclaise <jpritsk@purdue.edu>
2023-10-19 17:41:38 -0400

    Add MixCE

--- rank.py
+++ rank.py
@@ -2,21 +2,40 @@ from dataclasses import dataclass
 import math
 from datasets import Dataset
 from typing import Callable, Mapping
-from transformers import PreTrainedModel
+from transformers import PreTrainedModel, PreTrainedTokenizerBase
 from tqdm import trange
 from tqdm.contrib import tenumerate
 from torch.utils.data import DataLoader
 import torch
+from torch import nn
 from typing import Optional
 import wandb
 
 from .optim import OverlapOptimizer
 from .lr import LRScheduler
 
+def prepare_for_ranked(tokenizer: PreTrainedTokenizerBase, model: PreTrainedModel):
+    tokenizer.add_special_tokens(["[REF]"])
+    model.resize_token_embeddings(len(tokenizer))
+    return (tokenizer, model)
+
+def dpo_loss(pi_logps, ref_logps, beta):
+    pi_yw_logps,  pi_yl_logps =  pi_logps[:, 0],  pi_logps[:, 1:]
+    ref_yw_logps, ref_yl_logps = ref_logps[:, 0], ref_logps[:, 1:]
+
+    pi_logratios  = pi_yw_logps - pi_yl_logps
+    ref_logratios = ref_yw_logps - ref_yl_logps
+
+    losses = -F.logsigmoid(beta * (pi_logratios - ref_logratios))
+    rewards = beta * (pi_logps - ref_logps).detach()
+
+    return losses, rewards
+
 @dataclass
-class SlimTrainer():
+class SlimRankTrainer:
     model: PreTrainedModel
     optim: OverlapOptimizer
+    ref_token: string = "[REF]"
     train_data: Dataset
     epochs: int
     data_collator: Callable
@@ -26,25 +45,7 @@ class SlimTrainer():
     wandb_project: Optional[str]
     wandb_name: Optional[str]
     report_steps: int = 8
-    neft: bool = False # https://arxiv.org/abs/2310.05914
-    freeze_embeds: bool = True
-    mixce: bool = False # https://arxiv.org/abs/2305.16958
-    mixce_ratio: float = 0.2
-
-    def compute_loss(self, labels, inputs):
-        if self.mixce:
-            labels = labels.clone()
-            logits = self.model(**inputs).logits.log_softmax()
-            mask = (labels != -100)
-            log_probs = torch.gather(logits, dim=-1, index=labels.unsqueeze(-1)).squeeze(-1) * mask
-
-            with torch.no_grad():
-                q = torch.exp(log_probs.detach())
-
-            losses = self.mixce_ratio * log_probs + (1.0 - self.mixce_ratio) * q * log_probs
-            return losses.sum() / mask.sum()
-        else:
-            return self.model(**inputs).loss
+    neft: bool = False
 
     def train(self):
         first = True
@@ -56,9 +57,7 @@ class SlimTrainer():
         total_batches = len(loader)
         self.optim.init()
 
-        if self.freeze_embeds:
-            self.model.get_input_embeddings().requires_grad = False
-            self.model.get_output_embeddings().requires_grad = False
+        ref_id = tokenizer.convert_tokens_to_ids(self.ref_token)
 
         if self.neft:
             embedding_layer = self.model.get_input_embeddings()
@@ -72,21 +71,17 @@ class SlimTrainer():
                 if self.neft:
                     embeds = embedding_layer(batch['input_ids'].cuda())
                     noise = (torch.rand_like(embeds) - 0.5) * 10/math.sqrt(512 * embeds.shape[-1])
-                    loss = self.compute_loss(
-                        labels,
-                        inputs={
-                            'inputs_embeds': embeds + noise,
-                            'labels': batch['labels'].cuda()
-                        }
+                    losses = self.model(
+                        inputs_embeds=torch.where(batch['input_ids'] != ref_id, embeds + noise, embeds),
+                        labels=batch['labels'].cuda()
                     ).loss
                 else:
-                    loss = self.compute_loss(
-                        labels,
-                        inputs={
-                            'inputs_embeds': batch['input_ids'].cuda(),
-                            'labels': batch['labels'].cuda()
-                        }
+                    losses = self.model(
+                        input_ids=batch['input_ids'].cuda(),
+                        labels=batch['labels'].cuda()
                     ).loss
+
+
 
                 self.optim.step(loss, self.scheduler.get_lr()) # Backwards pass is mixed with optimization pass
                 self.scheduler.step()
