b6cd35a9437c8b764a2b600a8d987a43bc3f88df
euclaise <jpritsk@purdue.edu>
2023-11-16 12:25:32 -0500

    Encoder-decoder models

--- trainer.py
+++ trainer.py
@@ -93,7 +93,7 @@ class SlimTrainer():
                     loss = self.compute_loss(
                         labels=batch['labels'].cuda(),
                         input_ids = batch['input_ids'].cuda(),
-                        decoder_input_ids = None if not self.encdec else batch['decoder_input_ids'].cuda() if batch['decoder_input_ids'] is not None else None
+                        decoder_input_ids = None if not self.encdec else batch['decoder_input_ids'].cuda() if 'decoder_input_ids' in batch else None
                     )
 
                 self.optim.step(loss, self.scheduler.get_lr()) # Backwards pass is mixed with optimization pass
