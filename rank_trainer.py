632dfe380308a665e479a00622a061558516db4d
euclaise <jpritsk@purdue.edu>
2024-01-01 02:41:41 -0500

    Fix up PRO implementation and drop the rest

--- rank_trainer.py
+++ rank_trainer.py
@@ -5,215 +5,30 @@ import torch
 from torch import nn
 from typing import Dict
 
-def generate_pairs(n):
-    # Pos of i < pos of j, means that r(i) > r(j)
-    for i in range(n - 1):
-        for j in range(i + 1, n):
-            yield (i, j)
+def logprob(self, logits: torch.Tensor, labels: torch.LongTensor, normalize=False):
+    labels = labels.clone()
 
-class RankLoss:
-    def logprob(self, logits: torch.Tensor, labels: torch.LongTensor, normalize=False):
-        labels = labels.clone()
+    logits = logits[:, :-1, :].contiguous()
+    labels = labels[:, 1:].contiguous()
 
-        logits = logits[:, :-1, :].contiguous()
-        labels = labels[:, 1:].contiguous()
+    mask = (labels != -100)
+    labels[labels == -100] = 0
 
-        mask = (labels != -100)
-        labels[labels == -100] = 0
+    logprobs = logits.gather(dim=-1, index=labels.unsqueeze(-1)).squeeze(-1)
 
-        logprobs = logits.gather(dim=-1, index=labels.unsqueeze(-1)).squeeze(-1)
+    if normalize:
+        return (logprobs * mask).sum(dim=-1) / mask.sum(dim=-1)
+    else:
+        return (logprobs * mask).sum(dim=-1)
 
-        if normalize:
-            return (logprobs * mask).sum(dim=-1) / mask.sum(dim=-1)
-        else:
-            return (logprobs * mask).sum(dim=-1)
-
-class DPOLoss(RankLoss):
-    def __init__(model: nn.Module, beta: float = 1.0):
-        self.ref = copy.deepcopy(model)
-        self.model = model
-        self.beta = beta
-
-    def pair_loss(
-        self,
-        logits_w_ref: torch.Tensor,
-        logits_w_p: torch.Tensor,
-        logits_l_ref: torch.Tensor,
-        logits_l_p: torch.Tensor,
-        labels_w: torch.LongTensor,
-        labels_l: Torch.LongTensor
-    ):
-        logp_w_ref = self.logprob(logits_w_ref, lablels_w)
-        logp_w_p = self.logprob(logits_w_p, lablels_w)
-
-        logp_l_ref = self.logprob(logits_l_ref, lablels_l)
-        logp_l_p = self.logprob(logits_l_p, lablels_l)
-
-        left = self.beta * (logp_w_p - logp_w_ref)
-        right = self.beta * (logp_l_p - logp_w_ref)
-
-        return -F.logsigmoid(left - right)
-
-
-    def loss(self, labels: torch.Tensor, meta: Dict, **inputs):
+class PROTrainer(SlimTrainer):
+    def compute_loss(self, labels: torch.Tensor, meta: Dict, **inputs):
         # [bsz, candidates, seq_len]
-        k, v = [(ks, vs) for ks, vs in inputs.items()][0]
-        
-        n_cands = v.shape[1]
-
-        pair_losses = []
-        pair_valid = []
-
-        cache = dict()
-
-        for (i, j) in generate_pairs(n_cands):
-            if i in cache:
-                logits_i_ref, logits_i_p = cache[i]
-            else:
-                logits_i_ref = self.ref(**{k: v[:, i, ...]}).logits.log_softmax(dim=-1)
-                logits_i_p = self.model(**{k: v[:, i, ...]}).logits.log_softmax(dim=-1)
-                cache[i] = (logits_i_ref, logits_i_p)
-
-            if j in cache:
-                logits_j_ref, logits_j_p = cache[j]
-            else:
-                logits_j_ref = self.ref(**{k: v[:, j, ...]}).logits.log_softmax(dim=-1)
-                logits_j_p = self.model(**{k: v[:, j, ...]}).logits.log_softmax(dim=-1)
-                cache[j] = (logits_j_ref, logits_j_p)
-
-            pair_losses.append(self.pair_loss(
-                logits_i_ref,
-                logits_i_p,
-                logits_j_ref,
-                logits_j_p,
-                labels[:, i, ...],
-                labels[:, j, ...]
-            ))
-            pair_valid.append(meta['mask'][:, i] & meta['mask'][:, j])
-
-        pair_losses = torch.stack(pair_losses) * torch.stack(pair_valid)
-        return pair_losses.sum()
-        
-class IPOLoss(DPOLoss):
-    def __init__(model: nn.Module, tau: float = 1.0):
-        self.ref = copy.deepcopy(model)
-        self.model = model
-        self.tau = tau
-
-    def pair_loss(
-        self,
-        logits_w_ref: torch.Tensor,
-        logits_w_p: torch.Tensor,
-        logits_l_ref: torch.Tensor,
-        logits_l_p: torch.Tensor,
-        labels_w: torch.LongTensor,
-        labels_l: Torch.LongTensor
-    ):
-        logp_w_ref = self.logprob(logits_w_ref, lablels_w)
-        logp_w_p = self.logprob(logits_w_p, lablels_w)
-
-        logp_l_ref = self.logprob(logits_l_ref, lablels_l)
-        logp_l_p = self.logprob(logits_l_p, lablels_l)
-
-        h = (logp_w_p + logp_l_ref) - (logp_w_l + logp_w_ref)
-
-        return -((h - ((1 / self.tau) / 2)) ** 2)
-
-class cDPOLoss(DPOLoss):
-    def __init__(model: nn.Module, beta: float = 1.0, eps: float = 0.05):
-        self.ref = copy.deepcopy(model)
-        self.model = model
-        self.beta = beta
-        self.eps = eps
-
-    def pair_loss(
-        self,
-        logits_w_ref: torch.Tensor,
-        logits_w_p: torch.Tensor,
-        logits_l_ref: torch.Tensor,
-        logits_l_p: torch.Tensor,
-        labels_w: torch.LongTensor,
-        labels_l: Torch.LongTensor
-    ):
-        logp_w_ref = self.logprob(logits_w_ref, lablels_w)
-        logp_w_p = self.logprob(logits_w_p, lablels_w)
-
-        logp_l_ref = self.logprob(logits_l_ref, lablels_l)
-        logp_l_p = self.logprob(logits_l_p, lablels_l)
-
-        left = self.beta * (logp_w_p - logp_w_ref)
-        right = self.beta * (logp_l_p - logp_w_ref)
-
-        return self.eps * F.logsigmoid(left - right) + (1 - self.eps) * F.logsigmoid(right - left)
-
-class RRHFLoss:
-    def __init__(model: nn.Module):
-        self.model = model
-
-    def pair_loss(
-        self,
-        logits_w: torch.Tensor,
-        logits_l: torch.Tensor,
-        labels_w: torch.LongTensor,
-        labels_l: Torch.LongTensor
-    ):
-        logprob_w = self.logprob(logits_w, labels_w)
-        logp_w = self.logprob(logits_w, lablels_w, normalize=True)
-        logp_l = self.logprob(logits_l, lablels_l, normalize=True)
-
-        lm_loss = -logprob_w.sum()
-
-        ridge_loss = (logp_l - logp_w).clamp(min=0).sum()
-
-        return lm_loss + ridge_loss
-
-
-    def loss(self, labels: torch.Tensor, meta: Dict, **inputs):
-        # [bsz, candidates, seq_len]
-        k, v = [(ks, vs) for ks, vs in inputs.items()][0]
-        
-        n_cands = v.shape[1]
-
-        pair_losses = []
-        pair_valid = []
-
-        cache = dict()
-
-        for (i, j) in generate_pairs(n_cands):
-            if i in cache:
-                logits_i = cache[i]
-            else:
-                logits_i = self.model(**{k: v[:, i, ...]}).logits.log_softmax(dim=-1)
-                cache[i] = logits_i
-
-            if j in cache:
-                logits_j = cache[j]
-            else:
-                logits_j = self.model(**{k: v[:, j, ...]}).logits.log_softmax(dim=-1)
-                cache[j] = logits_j
-
-            pair_losses.append(self.pair_loss(
-                logits_i,
-                logits_j,
-                labels[:, i, ...],
-                labels[:, j, ...]
-            ))
-            pair_valid.append(meta['mask'][:, i] & meta['mask'][:, j])
-
-        pair_losses = torch.stack(pair_losses) * torch.stack(pair_valid)
-        return pair_losses.sum()
-
-class PROLoss:
-    def __init__(model: nn.Module):
-        self.model = model
-
-    def loss(self, labels: torch.Tensor, meta: Dict, **inputs):
-        # [bsz, candidates, seq_len]
-        k, v = [(ks, vs) for ks, vs in inputs.items()][0]
+        inkk, inv = [(ks, vs) for ks, vs in inputs.items()][0] # input_ids/input_embeds
 
         lm_loss = None
         logits = []
-        n_cands = v.shape[1]
+        max_n_cands = inv.shape[1]
 
         for i in range(n_cands):
             output = self.model(**{k: v[:, i, ...]})
@@ -223,33 +38,20 @@ class PROLoss:
 
             logits.append(output.logits.log_softmax(dim=-1))
 
-        logits = torch.stack(logits)
+        logits = torch.stack(logits, dim=1)
+        lps = [logprob(logits[:, i], labels[:, i]) for i in range(n_cands, normalize=True)]
+        lps = torch.stack(lps, dim=1)
 
         cand_losses = []
-        for i in range(n_cands - 1):
-            num = torch.exp(self.logprob(logits[i], labels[:, i, ...], normalize=True))
+        for i in range(lps.shape[1] - 1):
+            lognum = lps[:, i]
 
-            denom = torch.zeros_like(num)
+            den_lps = lps[:, i:]
+            den_lps = torch.where(meta['mask'][:, i:], den_lps, float('-inf'))
+            logdenom = torch.logsumexp(den_lps, dim=1)
 
-            for j in range(i, n_cands - 1):
-                denom += torch.exp(self.logprob(logits[j], labels[:, j, ...], normalize=True))
-
-            r = torch.log(num / denom)
-            
-            r *= meta['mask'][:, i] & meta['mask'][:, j]
+            r = (lognum - logdenom) * meta['mask'][:, i]
             cand_losses.append(r)
 
         r_loss = -torch.stack(cand_losses).sum()       
         return lm_loss + r_loss
-
-    
-@dataclass
-class RankedTrainer(SlimTrainer):
-    loss: RankLoss
-
-    def compute_loss(self, labels: torch.Tensor, meta: Dict, **inputs):
-        # [bsz, candidates, seq_len]
-        k, v = [(ks, vs) for ks, vs in inputs.items()][0]
-        assert meta is not None and 'mask' in meta, "Ranked modeling requires an ordering mask" 
-        
-        return self.loss.loss(labels, inputs, meta)
